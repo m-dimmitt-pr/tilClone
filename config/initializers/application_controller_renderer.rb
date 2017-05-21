a0b4a8e29f3ee7370c66a8d62c43af524f4c79ee
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-05-21 12:31:23 -0400

    play with heroku

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
 ApplicationController.renderer.defaults.merge!(
-  http_host: ENV['host'],
-  https: ENV['protocol'].include?('https')
+  http_host: 'http://pacific-badlands-34867.herokuapp.com',
+  https: true
 )
